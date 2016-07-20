760527d874ea9df54647bfa916ceebbfa94f548c
Alexis Gallèpe <alexis.gallepe@gmail.com>
2016-07-20 21:52:25 +0200

    cleansed code, almost ready to be a red black tree
    rotations left and right functionals

--- binary_tree.hsproj/HSbinary_tree.hs
+++ binary_tree.hsproj/HSbinary_tree.hs
@@ -1,36 +1,40 @@
 import System.IO  
+import System.Process
 
 -- Binary Tree
-data Tree a = Leaf
-             | Node a String (Tree a) (Tree a)
-             deriving (Show, Eq, Ord)   
 
-get_left:: Tree a -> Tree a
-get_left (Node _ _ left _)    = left
+data Color =  Black
+            | Red
+            deriving( Eq )
+            
+instance Show Color where
+  show Black = "black"
+  show Red = "red"
 
-get_right:: Tree a -> Tree a
-get_right  (Node _ _ _ right) = right
 
-get_id:: (Num a, Eq a,Ord a) => Tree a -> a
-get_id  Leaf = -1
-get_id  (Node a _ _ _) = a
+data Tree o =  Leaf
+             | Node  {
+                  object :: o ,
+                  nodeId :: Int,
+                  color :: Color,
+                  leftNode :: (Tree o),
+                  rightNode :: (Tree o),
+                  parentNode :: (Tree o)
+               }
+             deriving (Show)  
+             
+ 
 
-max_val:: (Eq a,Ord a) => Tree a -> a
-max_val (Node x _ _ Leaf) = x
-max_val (Node x _ _ right) = max_val right
+max_id:: Tree o -> Int
+max_id (Node _ id _ _ Leaf _) = id
+max_id (Node _ id _ _ right _) = max_id right
 
-min_val:: (Eq a,Ord a) => Tree a -> a
-min_val (Node x _ Leaf _) = x
-min_val (Node x _ left _) = min_val left
+min_id:: Tree o -> Int
+min_id (Node _ id _ Leaf _ _) = id
+min_id (Node _ id _ left _ _) = min_id left
       
-insert:: (Num a,Eq a,Ord a, Show a) => Tree a -> a -> Tree a
-insert  Leaf v = Node v (show v) Leaf Leaf
-insert (Node x y left right) v 
-        | v == x =  Node x y left right
-        | v > x  =  (Node x y left (insert right v))
-        | v < x  =  (Node x y (insert left v) right)
-        
-delete:: (Eq a,Ord a) => Tree a -> a -> Tree a
+ {-       
+delete:: (Eq o,Ord o) => Tree o -> o -> Tree o
 delete Leaf _ = Leaf
 delete (Node x y left right) v 
         | v == x = reorg (Node x y left right)
@@ -40,72 +44,110 @@ delete (Node x y left right) v
                 reorg (Node _ _ Leaf right) = right
                 reorg (Node _ _ left Leaf) = left
                 reorg (Node _ _ left right) = 
-                    let max = max_val left in
+                    let max = max_id left in
                     Node max y (delete left max) right
-            
+-}           
 
-list_to_tree:: (Num a,Eq a,Ord a, Show a) => [a] -> Tree a -> Tree a
+list_to_tree:: [Int] -> Tree String -> Tree String
 list_to_tree [] Leaf = Leaf
 list_to_tree [] tree = tree
-list_to_tree (x:xs) tree = list_to_tree xs (equilibre $ insert tree x)
+list_to_tree (x:xs) tree = list_to_tree xs (insert tree " " x)
 
-tree_to_list:: Tree a -> String
+tree_to_list:: Tree String -> String
 tree_to_list Leaf = []
-tree_to_list (Node _ y left right) =
-    (tree_to_list left) ++ y ++ "\n" ++ (tree_to_list right)
+tree_to_list (Node _ id color left right _) =
+    (tree_to_list left) ++ (show id) ++ "\n" ++ (tree_to_list right)
         
-invert_tree:: Tree a -> Tree a
+invert_tree:: Tree o -> Tree o
 invert_tree Leaf = Leaf
-invert_tree (Node x y left right) =
-     Node x y (invert_tree right) (invert_tree left)
+invert_tree t@(Node o id color left right parent) =
+     Node o id color (invert_tree right) (invert_tree left) t
 
-is_value_present:: (Eq a,Ord a) => a -> Tree a -> Bool
-is_value_present _ Leaf = False
-is_value_present v (Node x _ left right) 
-    | x == v = True
-    | v > x  = is_value_present v right
-    | v < x  = is_value_present v left
+get_node_by_id::Tree o -> Int -> Tree o
+get_node_by_id Leaf _ = Leaf
+get_node_by_id node@(Node o parentId color left right _) id
+    | id == parentId = node
+    | id > parentId  = get_node_by_id right id
+    | id < parentId  = get_node_by_id left id
     
 
-equilibre :: (Num a, Eq a,Ord a, Show a) =>Tree a -> Tree a
-equilibre (Node x y (Node lx ly lleft (Node lrx lry lrleft lrright)) right)     
-        | lx < x && lx < lrx && x > lrx= Node lrx lry (Node lx ly lleft lrleft) (Node x y lrright right)    
-        | otherwise = (Node x y (Node lx ly lleft (Node lrx lry lrleft lrright)) right)
-        
-equilibre (Node x y left (Node rx ry (Node rlx rly rlleft rlright) rright))     
-        | x < rx && rx > rlx && x < rlx = Node rlx rly (Node x y left rlleft) (Node rx ry rlright rright)
-        | otherwise = (Node x y left (Node rx ry (Node rlx rly rlleft rlright) rright))  
-        
-equilibre (Node x y (Node lx ly lleft lright) right)     
-        | x > lx && lx > (get_id lleft) && ((get_id lleft) /= -1) = Node lx ly lleft (Node x y lright right)
-        | otherwise = (Node x y (Node lx ly lleft lright) right) 
-        
-equilibre (Node x y left (Node rx ry rleft rright)) 
-        | x < rx && rx < (get_id rright) && ((get_id rright) /= -1) = Node rx ry (Node x y left rleft) rright
-        | otherwise = (Node x y left (Node rx ry rleft rright)) 
-   
-equilibre a = a
--- | otherwise = (Node x y (Node lx ly lleft lright) (Node rx ry rleft rright)) 
 
-arcs :: Tree a -> [(String,String)]
+-- RedBlack Tree poperties
+
+rotateLeft :: Tree o -> Tree o
+rotateLeft t@(Node oX idX cX leftX (Node oY idY cY leftY rightY _) parent) = Node oY idY cY (Node oX idX cX leftX leftY t) rightY parent
+rotateLeft n = n
+  
+rotateRight :: Tree o -> Tree o
+rotateRight t@(Node oY idY cY (Node oX idX cX leftX rightX _) rightY parent) = Node oX idX cX leftX (Node oY idY cY rightX rightY t) parent
+rotateRight n = n
+
+
+
+insert::(Ord a)=>a->Tree a->Tree a
+insert x Leaf = Node x Leaf Leaf Leaf
+insert x t@(Node a l r p) = insertIt x t p
+ where insertIt x Leaf (Node a l r p) = Node x Leaf Leaf (Node a Leaf Leaf Leaf)--1*
+       insertIt x t@(Node a l r p) parent
+        | x == a = t
+        | x <  a = Node a (insertIt x l t) r p
+        | x >  a = Node a l (insertIt x l t) p
+
+insert:: Tree o -> o -> Int -> Tree o
+insert  Leaf o id = Node o id Red Leaf Leaf Leaf
+insert current@(Node oParent idParent color left right parent) o id = insert2 current o id parent
+  where insert2 Leaf o2 id2 parent2 
+        insert2 current2@(Node oP idP c l r p) o2 id2 parent2 
+        | id2 == idP = current2
+        | id > idParent  =  (Node oParent idParent color left (insert2 right o id current) parent)
+        | id < idParent  =  (Node oParent idParent color (insert2 left o id current) right parent)
+
+    
+
+
+{-
+insert_fixup node@(Node o id c left right parent) =  
+    if (color parent) == Red then
+      node
+    else
+      
+      if parent == (leftNode (parentNode parent))
+
+
+
+
+
+
+
+
+
+
+-}
+
+
+
+-- To dotise
+arcs :: Tree o -> [(Int,Int)]
 arcs Leaf = []
-arcs (Node x y Leaf Leaf) = []
-arcs (Node x y  Leaf (Node x3 y3 left2 right2)) = [(y,y3)] ++ arcs Leaf ++ arcs (Node x3 y3 left2 right2)
-arcs (Node x y  (Node x2 y2 left right) Leaf) = [(y,y2)] ++ arcs(Node x2 y2 left right)  ++ arcs Leaf
-arcs (Node x y  (Node x2 y2 left right) (Node x3 y3 left2 right2)) = [(y,y2)] ++ [(y,y3)] ++ arcs(Node x2 y2 left right) ++ arcs(Node x3 y3 left2 right2)
+arcs (Node o id c Leaf Leaf _) = []
+arcs (Node o id c Leaf rightChild@(Node oRight idRight _ _ _ _) _) = [(id,idRight)] ++ (arcs rightChild)
+arcs (Node o id c leftChild@(Node oLeft idLeft _ _ _ _) Leaf _) = [(id,idLeft)] ++ arcs leftChild
+arcs (Node o id c leftChild@(Node oLeft idLeft _ _ _ _) rightChild@(Node oRight idRight _ _ _ _) _ ) = [(id,idLeft)] ++ [(id,idRight)] ++ arcs leftChild ++ arcs rightChild
 
 
-arc :: (String,String) -> String
-arc (a,b) = a ++ "->" ++ b
+arc :: (Int,Int) -> String
+arc (a,b) = show a ++ "->" ++ show b
 
-display_arcs :: Tree a -> String
+display_arcs :: Tree o -> String
 display_arcs tree = unlines ( map arc (arcs tree) )
 
-dotise :: Tree a -> String
+dotise :: Tree String -> String
 dotise tree = "digraph \"complet3\" { \n node [fontname=\"DejaVu-Sans\", shape=circle] \n \n"++  (tree_to_list tree) ++ "\n" ++ (display_arcs tree) ++ " }"
 
 makeTree = do        
-    writeFile "tree.dot" (dotise (equilibre $ list_to_tree ([0..20]) Leaf))
+    writeFile "/Users/alexisgallepe/Documents/Haskell-Binary-Tree/tree.dot" (dotise (list_to_tree ([20,19..0]) Leaf))
+    
+main = makeTree
 
 
 
